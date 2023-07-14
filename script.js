7e1ca5f514f0587a0ca0112dc392b58ad4da8ff9
Davit14 <davit.sahakyan4@tumo.org>
2023-07-14 18:46:11 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// function kalkulatr(a) {
+//     if (a % 2 === 0) {
+//         return `es zuyg em : ${a}`
+//     } else {
+//         return `es kent em :${a}`
+//     }
+
+// }
+// let patasxan = kalkulatr(44)
+// console.log(patasxan);
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// function calc(a) {
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] < 5) {
+//             console.log(`ha`)
+
+//         } else {
+//             console.log(`che`)
+//         }
+
+
+//     }
+// }
+
+// calc(arr)
+
+// let a = [1,2,3,4,5,6]
+
+// function Element(c){
+//     let x = Math.round(Math.random(c) *5)
+// return c[x]
+// }
+// let b = Element(a)
+// console.log(b)
+
+
+// let Davlo =  {
+//     anun : "Davit",
+//     tariq : 14,
+//       bnakavayr : "Hatis"
+// }
+
+// console.log(Davlo.bnakavayr);
